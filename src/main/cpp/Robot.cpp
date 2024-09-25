acd6b68de9b568bed007f1baaa21a498c3d11a49
tssmith7 <tssmith7@users.noreply.github.com>
2024-09-25 10:22:21 -0700

    Cleanup unused code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -11,16 +11,9 @@ void Robot::RobotInit() {
   // m_shooterMotor.SetInverted(true);
   // m_shooterMotor.EnableVoltageCompensation(12);
 
-  // ctre::phoenix6::configs::CANcoderConfiguration absoluteEncoderConfigs{};
-  // absoluteEncoderConfigs.MagnetSensor.MagnetOffset = 0.301;
-  // m_shooterEncoder.GetConfigurator().Apply(absoluteEncoderConfigs, 50_ms);
-
   // m_topShooterMotor.SetInverted(true);
   // m_bottomShooterMotor.SetInverted(true);
   // m_bottomShooterMotor.Follow(m_topShooterMotor);
-  // constexpr double kShooterAbsoluteOffset = -0.068;
-  // constexpr double kWristAbsoluteOffset = 0.314;
-  // constexpr double kArmAbsoluteOffset = -0.243;
 
   m_armEncoder.SensorDirection( true );
   m_wristEncoder.SensorDirection( true );
@@ -28,25 +21,11 @@ void Robot::RobotInit() {
   m_armMotor.SetInverted( true );
   m_armMotor.SetMotionProfile( armUI.GetMotionProfile() );
 
-  m_wristMotor.LinkCANCoder( m_wristEncoder.GetChannel() );
+  m_wristMotor.LinkCANCoder( m_wristEncoder.GetChannel(), 36 * 42.0 / 38.0 /* Wrist Gear Ratio */ );
   m_wristMotor.SetMotionProfile( wristUI.GetMotionProfile() );
-
-
-  // TuningParameters::Values pid_vals;
-  // // pid_vals = TuningParameters::Values{ kElevatorP, kElevatorI, kElevatorD, 
-  // //                                    kElevatorS, kElevatorG, kElevatorV, kElevatorA };
-  // // TuningParameters::SetSmartDashboardValues( "Elevator", pid_vals );
 
   ArmWristG = kArmWristG;
   frc::SmartDashboard::PutNumber( "ArmWristG", ArmWristG ); 
-
-  // frc::SmartDashboard::PutNumber( "Shooter P", kShooterP );
-  // frc::SmartDashboard::PutNumber( "Shooter I", kShooterI );
-  // frc::SmartDashboard::PutNumber( "Shooter D", kShooterD );
-  // frc::SmartDashboard::PutNumber( "Shooter S", kShooterS );
-  // frc::SmartDashboard::PutNumber( "Shooter G", kShooterG );
-  // frc::SmartDashboard::PutNumber( "Shooter V", kShooterV );
-  // frc::SmartDashboard::PutNumber( "Shooter A", kShooterA );
 
   frc::SmartDashboard::PutBoolean("Config Gains", false);
   frc::SmartDashboard::PutBoolean("Set Arm Zeros", false);
@@ -55,33 +34,18 @@ void Robot::RobotInit() {
 
 void Robot::RobotPeriodic() {
 
-  // m_shooterPosition = m_shooterEncoder.GetPosition().GetValueAsDouble() * 360_deg;
   m_wristAngle = m_wristEncoder.GetPosition();
   phi = m_armEncoder.GetPosition();
 
   m_armAngle = m_wristAngle - phi;
 
-  // frc::SmartDashboard::PutNumber("Wrist Angle", m_wristAngle.value());
   frc::SmartDashboard::PutNumber("Phi", phi.value());
-  // frc::SmartDashboard::PutNumber("Arm Angle", m_armAngle.value());
 
     // The extra kG needd for the arm based on the wrist angle.
   double armExtraFeedforward = ArmWristG * units::math::cos(m_wristAngle).value();
 
   m_armMotor.Update( m_armAngle, armExtraFeedforward );
-  m_wristMotor.Update();
-
-
-  // frc::SmartDashboard::PutNumber("Wrist Raw Position", m_wristEncoder.GetRawPosition());
-  // frc::SmartDashboard::PutNumber("Arm Raw Position", m_armEncoder.GetRawPosition());
-
-  // double elevatorP = frc::SmartDashboard::GetNumber( "Elevator P", 0.0 );
-  // double elevatorI = frc::SmartDashboard::GetNumber( "Elevator I", 0.0 );
-  // double elevatorD = frc::SmartDashboard::GetNumber( "Elevator D", 0.0 );
-  // double elevatorG = frc::SmartDashboard::GetNumber( "Elevator G", 0.0 );
-  // double elevatorV = frc::SmartDashboard::GetNumber( "Elevator V", 0.0 );
-  // double elevatorS = frc::SmartDashboard::GetNumber( "Elevator S", 0.0 );
-  // double elevatorA = frc::SmartDashboard::GetNumber( "Elevator A", 0.0 );
+  m_wristMotor.Update( m_wristEncoder.GetPosition() );
 
   bool configGains = frc::SmartDashboard::GetBoolean("Config Gains", false);
   if (configGains) {
@@ -104,37 +68,7 @@ void Robot::RobotPeriodic() {
 
     frc::SmartDashboard::PutBoolean("Set Arm Zeros", false);
     fmt::print(" Encoders Zeroed...\n" );
-}
-
-  // double shooterP = frc::SmartDashboard::GetNumber( "Shooter P", 0.0 );
-  // double shooterI = frc::SmartDashboard::GetNumber( "Shooter I", 0.0 );
-  // double shooterD = frc::SmartDashboard::GetNumber( "Shooter D", 0.0 );
-  // double shooterG = frc::SmartDashboard::GetNumber( "Shooter G", 0.0 );
-  // double shooterV = frc::SmartDashboard::GetNumber( "Shooter V", 0.0 );
-  // double shooterS = frc::SmartDashboard::GetNumber( "Shooter S", 0.0 );
-  // double shooterA = frc::SmartDashboard::GetNumber( "Shooter A", 0.0 );
-
-  // if ( elevatorP != kElevatorP ) { kElevatorP = elevatorP; m_elevatorPID.SetP( kElevatorP ); }
-  // if ( elevatorI != kElevatorI ) { kElevatorI = elevatorI; m_elevatorPID.SetI( kElevatorI ); }
-  // if ( elevatorD != kElevatorD ) { kElevatorD = elevatorD; m_elevatorPID.SetD( kElevatorD ); }
-
-  // if ( shooterP != kShooterP ) { kShooterP = shooterP; m_shooterPID.SetP( kShooterP ); }
-  // if ( shooterI != kShooterI ) { kShooterI = shooterI; m_shooterPID.SetI( kShooterI ); }
-  // if ( shooterD != kShooterD ) { kShooterD = shooterD; m_shooterPID.SetD( kShooterD ); }
-  
-  
-
-  // frc::SmartDashboard::PutNumber("Shooter Position", m_shooterPosition.value());
-
-
-
-
-  // if ( frc::DriverStation::IsDisabled() ) {
-  //   m_shooterSetpoint.position = m_shooterPosition;
-  //   m_shooterSetpoint.velocity = 0_deg_per_s;
-  //   m_shooterAngleGoal = m_shooterPosition;
-  // }
-
+  }
 }
 
 
@@ -166,21 +100,18 @@ void Robot::TeleopPeriodic() {
 
   
    if(m_xbox.GetAButton()){
-    m_armGoal = {-8_deg, 0_deg_per_s}; 
-    m_wristGoal = {-35_deg, 0_deg_per_s};
+    m_armMotor.SetGoal( {-8_deg, 0_deg_per_s} ); 
+    m_wristMotor.SetGoal( {-35_deg, 0_deg_per_s} ); 
   } else if (m_xbox.GetBButton()) {
-    m_armGoal = {170_deg, 0_deg_per_s}; 
-    m_wristGoal = {35_deg, 0_deg_per_s};
-  // } else if (m_xbox.GetXButton()) {
-  //   m_armGoal = {0_deg, 0_deg_per_s}; 
+    m_armMotor.SetGoal( {155_deg, 0_deg_per_s} ); 
+    m_wristMotor.SetGoal( {40_deg, 0_deg_per_s} ); 
+  } else if (m_xbox.GetXButton()) {
+    m_armMotor.SetGoal( {0_deg, 0_deg_per_s} ); 
+    m_wristMotor.SetGoal( {0_deg, 0_deg_per_s} ); 
   } else if (m_xbox.GetYButton()) {
-    m_armGoal = {160_deg, 0_deg_per_s}; 
-    m_wristGoal = {130_deg, 0_deg_per_s};
+    m_armMotor.SetGoal( {160_deg, 0_deg_per_s} ); 
+    m_wristMotor.SetGoal( {130_deg, 0_deg_per_s} ); 
   }
-
-  m_armMotor.SetGoal( m_armGoal );
-  m_wristMotor.SetGoal( m_wristGoal );
- 
 }
 
 
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -30,91 +30,29 @@ class Robot : public frc::TimedRobot {
   void TeleopPeriodic() override;
 
  private:
-  // double kElevatorP = 0.0;
-  // double kElevatorI = 0.0;
-  // double kElevatorD = 0.0;
-  // double kElevatorS = 0.0;
-  // double kElevatorG = 0.0;
-  // double kElevatorV = 0.0;
-  // double kElevatorA = 0.0;
-
 
   tuning::Parameters kArm{ 0.006, 0.0, 0.0, 0.0, 0.3, 1.1, 0.0 };
   tuning::AngularMotionProfile kArm_MP{540_deg_per_s, 720_deg_per_s_sq, 0_deg_per_s_cu};
   tuning::AngularTuningUI armUI{ "ArmSubsystem", "Arm", kArm, kArm_MP };
 
-  tuning::Parameters kWrist{ 1.0, 0.0, 0.0, 0.0, 0.0115, 0.65, 0.0 };
+  tuning::Parameters kWrist{ 1.0, 0.0, 0.0, 0.005, 0.018, 0.38, 0.0 };
   double kArmWristG = 0.01;
   tuning::AngularMotionProfile kWrist_MP{540_deg_per_s, 1080_deg_per_s_sq, 2000_deg_per_s_cu};
   tuning::AngularTuningUI wristUI{ "ArmSubsystem", "Wrist", kWrist, kWrist_MP };
 
- 
-  // double kShooterP = 0.01;
-  // double kShooterI = 0.0;
-  // double kShooterD = 0.0;
-  // double kShooterS = 0.0;
-  // double kShooterG = 0.1;
-  // double kShooterV = 8.0;
-  // double kShooterA = 0.0;
-
-  
-  // frc::TrapezoidProfile<units::meters> m_elevatorProfile{{1_mps, 1_mps_sq}};
-  // frc::TrapezoidProfile<units::meters>::State m_elevatorGoal;
-  // frc::TrapezoidProfile<units::meters>::State m_elevatorSetpoint;
-
-  // units::meter_t m_elevatorHeightGoal = 0_m;
-  // units::meter_t m_elevatorPosition;
-
-  // bool offsetsSet = false;
-
-
   AngularTalonFXTuner m_wristMotor{ "Wrist", 22, "", kWrist, tuning::Arm, tuning::OnBoard};
-  CTRECANcoder m_wristEncoder{ "Wrist Encoder", 24 };
+
+  CTRECANcoder m_wristEncoder{ "WristOffset", 24 };
 
   AngularTalonFXTuner m_armMotor{ "Arm", 21, "", kArm, tuning::Arm, tuning::Software };
-  CTRECANcoder m_armEncoder{ "Arm Encoder", 23 };
-
- // frc::PIDController m_wristPID{ kWristP, kWristI, kWristD };
- // frc::ArmFeedforward m_wristFeedforward{ units::volt_t{ kWristS }, units::volt_t{ kWristG }, 
- //                                       units::unit_t<frc::ArmFeedforward::kv_unit> { kWristV }, 
- //                                       units::unit_t<frc::ArmFeedforward::ka_unit> { kWristA } };
-
- // frc::TrapezoidProfile<units::degrees> m_wristProfile{{360_deg_per_s, 360_deg_per_s_sq}};
-  // frc::TrapezoidProfile<units::degrees>::State m_wristSetpoint;
-  frc::TrapezoidProfile<units::degrees>::State m_wristGoal;
+  CTRECANcoder m_armEncoder{ "ArmOffset", 23 };
 
   units::degree_t m_wristAngle;
   
-  // frc::TrapezoidProfile<units::degrees> m_armProfile{{360_deg_per_s, 360_deg_per_s_sq}};
-  frc::TrapezoidProfile<units::degrees>::State m_armGoal;
-  // frc::TrapezoidProfile<units::degrees>::State m_armSetpoint{};
-
   units::degree_t m_armAngle;
   units::degree_t phi;
  
   double ArmWristG;
-
-
-  // rev::CANSparkFlex m_topShooterMotor{14, rev::CANSparkFlex::MotorType::kBrushless};
-  // rev::CANSparkFlex m_bottomShooterMotor{15, rev::CANSparkFlex::MotorType::kBrushless};
-
-  // rev::CANSparkMax m_shooterMotor{19, rev::CANSparkMax::MotorType::kBrushless};
-  // ctre::phoenix6::hardware::CANcoder m_shooterEncoder{20};
-
-  // rev::SparkMaxRelativeEncoder m_shooterVel = m_shooterMotor.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor);
-
-  // frc::PIDController m_shooterPID{kShooterP, kShooterI, kShooterD};
-  // frc::ArmFeedforward m_shooterFeedforward{units::volt_t{kShooterS}, units::volt_t{kShooterG}, 
-  //                                       units::unit_t<frc::ArmFeedforward::kv_unit> {kShooterV}, 
-  //                                       units::unit_t<frc::ArmFeedforward::ka_unit> {kShooterA}};
-  
-  // frc::TrapezoidProfile<units::degrees> m_shooterProfile{{360_deg_per_s, 360_deg_per_s_sq}};
-  // frc::TrapezoidProfile<units::degrees>::State m_shooterGoal;
-  // frc::TrapezoidProfile<units::degrees>::State m_shooterSetpoint{};
-
-  // units::degree_t m_shooterAngleGoal;
-  // units::degree_t m_shooterPosition;
-
 
   frc::XboxController m_xbox{1};
 };
